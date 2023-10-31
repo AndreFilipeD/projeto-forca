d9c7844c9328ffb2fb4428ace3d70a9ac686a832
André Filipe <117784318+AndreFilipeD@users.noreply.github.com>
2023-10-31 18:29:19 -0300

    Adição de conteúdo

--- javascript.js
+++ javascript.js
@@ -2,24 +2,37 @@ var selectedWord
 let forcatext = window.document.querySelector("#forcatext")
 var categAnimal = ['alce','avestruz','babuino','cachorro','coelho','cabra','cegonha','elefante','foca','flamingo','gato','gorila','girafa','iguana','jaguar','javali','kiwi','lagartixa','macaco','mamute','ornitorrinco','porco','quati','rato','raposa','ratazana','sapo','sardinha','texugo','urso','urubu','vaca','veado','vibora','zebra']
 var winChars = []
+var splitChars = []
 
 function newgame(){
-    winChars = []
     selectedWord = categAnimal[Math.floor(Math.random() * categAnimal.length)].toUpperCase();
-    for(var c of selectedWord){
-        forcatext.innerHTML +='_ '
+    splitChars = selectedWord.split("")
+    for(c of selectedWord){
+        winChars.push(0)
+        forcatext.innerHTML += '_ '
     }
-    
 }
+
 function action(str){
-    forcatext.innerHTML = ''
-    for(var c of selectedWord){
-        if(c === str || winChars.some(c)){
-            forcatext.innerHTML += str
-            winChars.push(c)
-        }else{
-            forcatext.innerHTML +='_'
-        }
-    }
+    winChars = selectedWord.split("")
+    window.alert(splitChars)
     window.alert(winChars)
+    /*
+    for(c of selectedWord){
+        window.alert(selectedWord[c])
+    }
+    */
 }
+    /*forcatext.innerHTML = ''//Limpa texto visual
+    for(var c of selectedWord){//Percorre toda palavra
+        if(winChars[c]==='_'){//Verifica se já foi encontrado
+            if(str===selectedWord[c]){//Verifica se é a letra correta
+                winChars[c]=str//Adiciona nova letra na memória
+                forcatext.innerHTML += str+' '//Escreve nova letra descoberta
+            }else{//Caso nova letra não seja correta
+                forcatext.innerHTML +='_ '//Adiciona espaço visual em branco
+            }
+        }else{//Caso já tenha sido encontrado
+            forcatext.innerHTML += winChars[c]//Mostre a letra na memória
+        }
+    }*/
